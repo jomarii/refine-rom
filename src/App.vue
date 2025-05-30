a85f579836f9b490042ff28704cd89badd13b06b
Jomari <arahanjomari07@gmail.com>
2025-05-31 01:18:37 +0800

    rating rework

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,74 @@
 <template>
-  <Refine />
+    <div class="flex flex-col gap-2 mt-10 border border-gray-300 rounded-md p-4 bg-white">
+      <div class="flex flex-row gap-2 mb-10">
+          <div class="flex flex-row gap-2">
+              <span class="text-gray-500 my-auto">Zeny</span>
+              <input class="border border-gray-300 rounded-md p-2 outline-none" type="text" v-model="zeny" />
+          </div>
+          <div class="flex flex-row gap-2">
+              <span class="text-gray-500 my-auto">Main Eq Price</span>
+              <input class="border border-gray-300 rounded-md p-2 outline-none" type="text" v-model="mainEq" />
+          </div>
+          <div class="flex flex-row gap-2">
+              <span class="text-gray-500 my-auto">Alay Eq Price</span>
+              <input class="border border-gray-300 rounded-md p-2 outline-none" type="text" v-model="alayEq" />
+          </div>
+          <div class="flex flex-row gap-2">
+              <span class="text-gray-500 my-auto">Expense {{ formattedExpense }}</span>
+          </div>
+      </div>
+      <div class="flex flex-row space-x-10">
+          <div class="flex flex-col gap-2">
+              <span class="text-gray-500 my-auto">Main</span>
+              <Refine :zeny="parseInt(zeny)" :equipment="parseInt(mainEq)" @update:zeny="updateZeny" @update:expense="updateExpense" @update:eqRepair="updateMainEqRepair" />
+          </div>
+          <div class="flex flex-col gap-2">
+              <span class="text-gray-500 my-auto">Alay</span>
+              <Refine :zeny="parseInt(zeny)" :equipment="parseInt(alayEq)" @update:zeny="updateZeny" @update:expense="updateExpense" @update:eqRepair="updateAlayEqRepair" />
+          </div>
+      </div>
+      <div class="border border-gray-300 rounded-md p-4">
+        <div class="flex flex-row gap-2">
+            <span class="text-gray-500 my-auto">Main Repair {{ mainEqRepair }}</span>
+            <span class="text-gray-500 my-auto">Alay Repair {{ alayEqRepair }}</span>
+        </div>
+        <div class="flex flex-row gap-2">
+            
+        </div>
+      </div>
+    </div>
 </template>
 
 <script setup>
 import Refine from './components/Refine.vue'
+
+import { ref, computed } from 'vue'
+
+const zeny = ref(1000000)
+const expense = ref(0)
+
+const mainEq = ref(0)
+const alayEq = ref(0)
+const mainEqRepair = ref(0)
+const alayEqRepair = ref(0)
+
+const updateZeny = (newZeny) => {
+    zeny.value = newZeny
+}
+
+const updateExpense = (exp) => {
+    expense.value += parseInt(exp)
+}
+
+const formattedExpense = computed(() => {
+      return new Intl.NumberFormat('en-US').format(expense.value);
+})
+
+const updateMainEqRepair = (newMainEqRepair) => {
+    mainEqRepair.value += parseInt(newMainEqRepair)
+}
+
+const updateAlayEqRepair = (newAlayEqRepair) => {
+    alayEqRepair.value += parseInt(newAlayEqRepair)
+}
 </script>
--- src/components/Refine.vue
+++ src/components/Refine.vue
@@ -1,49 +1,130 @@
 <template>
     <div class="flex flex-col border border-gray-300 rounded-md p-4 space-x-4">
-        <div class="m-auto flex flex-row gap-2 mb-10">
-            <div class="flex flex-row gap-2">
-                <span class="text-gray-500 my-auto">Zeny</span>
-                <input class="border border-gray-300 rounded-md p-2 outline-none" type="text" v-model="zeny" />
-            </div>
-            <div class="flex flex-row gap-2">
-                <span class="text-gray-500 my-auto">Equipment Price</span>
-                <input class="border border-gray-300 rounded-md p-2 outline-none" type="text" v-model="equipment" />
-            </div>
-        </div>
+        
         <div class="flex flex-col gap-2 m-auto">
             <div class="flex gap-2">
                 <div class="rounded-full w-30 h-30 flex" :class="resultClass">
                     <span class="m-auto text-4xl">{{ refineCount }}</span>
                 </div>
             </div>
-            <button class="border text-white rounded-md p-2 cursor-pointer" :class="isDisabled ? 'bg-gray-300 border-gray-300' : 'bg-amber-400 border-amber-500'" @click="refine" v-if="!isBroken" :disabled="isDisabled">Refine</button>
-            <button class="bg-red-400 text-white rounded-md p-2 border border-red-500 cursor-pointer" @click="repair" v-else>Repair</button>
+            <button class="border text-white rounded-md p-2 cursor-pointer" :class="isDisabledRefine ? 'bg-gray-300 border-gray-300' : 'bg-amber-400 border-amber-500'" @click="refine" v-if="!isBroken" :disabled="isDisabledRefine">Refine</button>
+            <button class="text-white rounded-md p-2 border cursor-pointer" :class="isDisabledRepair ? 'bg-gray-300 border-gray-300' : 'bg-red-400 border-red-500'" @click="repair" v-else :disabled="isDisabledRepair">Repair</button>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref, reactive, computed } from 'vue'
+import { ref, computed, reactive } from 'vue'
+
+const emit = defineEmits(['update:zeny', 'update:expense', 'update:mainEqRepair', 'update:eqRepair'])
+
+const props = defineProps({
+    zeny: {
+        type: Number,
+        required: true
+    },
+    equipment: {
+        type: Number,
+        required: true
+    }
+})
 
 const refineCount = ref(0)
-const zeny = ref(1000000)
-const equipment = ref(0)
+
+const refineRating = reactive({
+    1: {
+        success: 1,
+        failed: 0,
+        break: 0
+    },
+    2: {
+        success: 1,
+        failed: 0,
+        break: 0
+    },
+    3: {
+        success: 1,
+        failed: 0,
+        break: 0
+    },
+    4: {
+        success: 0.51,
+        failed: 0.28,
+        break: 0.21
+    },
+    5: {
+        success: 0.49,
+        failed: 0.25,
+        break: 0.26
+    },
+    6: {
+        success: 0.39,
+        failed: 0.29,
+        break: 0.32
+    },
+    7: {
+        success: 0.42,
+        failed: 0.30,
+        break: 0.28
+    },
+    8: {
+        success: 0.46,
+        failed: 0.28,
+        break: 0.26
+    },
+    9: {
+        success: 0.41,
+        failed: 0.31,
+        break: 0.28
+    },
+    10: {
+        success: 0.40,
+        failed: 0,
+        break: 0.60
+    },
+    11: {
+        success: 0.40,
+        failed: 0,
+        break: 0.60
+    },
+    12: {
+        success: 0.40,
+        failed: 0,
+        break: 0.60
+    },
+    13: {
+        success: 0.40,
+        failed: 0,
+        break: 0.60
+    },
+    14: {
+        success: 0.40,
+        failed: 0,
+        break: 0.60
+    },
+    15: {
+        success: 0.40,
+        failed: 0,
+        break: 0.60
+    }
+})
+
 const zenyPerRefine = ref([
-    10000,
-    20000,
-    30000,
-    40000,
-    50000,
-    60000,
-    70000,
-    80000,
-    90000,
-    100000,
-    100000,
-    100000,
-    100000,
-    100000,
-    100000
+    15000,
+    25000,
+    35000,
+    45000,
+    55000,
+    65000,
+    75000,
+    85000,
+    95000,
+    105000,
+    125000,
+    125000,
+    125000,
+    125000,
+    125000
 ])
 
 const isBroken = ref(false)
@@ -58,16 +139,12 @@ const resultClass = computed(() => {
     return 'bg-red-300'
 })
 
-const isDisabled = computed(() => {
-    return zeny.value < zenyPerRefine.value[refineCount.value] || refineCount.value == 15
+const isDisabledRefine = computed(() => {
+    return props.zeny < zenyPerRefine.value[refineCount.value] || refineCount.value == 15
 })
 
-const successRate = computed(() => {
-    return refineCount.value > 5 ? 0.4 : 0.5
-})
-
-const breakRate = computed(() => {
-    return refineCount.value > 5 ? 0.1 : 0.2
+const isDisabledRepair = computed(() => {
+    return props.zeny < props.equipment
 })
 
 const refine = () => {
@@ -75,42 +152,63 @@ const refine = () => {
         return
     }
 
-    zeny.value -= zenyPerRefine.value[refineCount.value]
+    emit('update:zeny', props.zeny - zenyPerRefine.value[refineCount.value])
+    emit('update:expense', zenyPerRefine.value[refineCount.value])
+
+    refineResult()
 
     if(refineCount.value < 4){
         result.value = 'success'
         refineCount.value++
         return
     }
-    
-    const success = Math.random() < successRate.value
-    console.log(`Success Rate: ${successRate.value}`)
-    if(isDisabled.value){
+
+    if(isDisabledRefine.value){
         return
     }
 
-    if (success) {
-        result.value = 'success'
+    if (result.value == 'success') {
         refineCount.value++
         return
     }
-    
+
     refineCount.value--
 
-    console.log(`Break Rate: ${breakRate.value}`)
-    const isBreak = Math.random() < breakRate.value
-    if(isBreak){
+    if(result.value == 'broken'){
         isBroken.value = true
-        result.value = 'broken'
         return
     }
-
-    result.value = 'failed'
 }
 
 const repair = () => {
-    zeny.value -= equipment.value
+    emit('update:zeny', props.zeny - props.equipment)
+    emit('update:expense', props.equipment)
+    emit('update:eqRepair', 1)
     isBroken.value = false
     result.value = 'repaired'
+}
+
+const refineResult = () => {
+    const rand = Math.random()
+    let successRate = refineRating[refineCount.value + 1].success
+    let failedRate = refineRating[refineCount.value + 1].failed
+    let breakRate = refineRating[refineCount.value + 1].break
+
+    console.log(`rand: ${rand}`)
+    console.log(`successRate: ${successRate}`)
+    console.log(`failedRate: ${failedRate}`)
+    console.log(`breakRate: ${breakRate}`)
+
+    if(rand < successRate){
+        result.value = 'success'
+        return
+    }
+    else if(rand < successRate + failedRate){
+        result.value = 'failed'
+        return
+    }
+    else if(rand < successRate + failedRate + breakRate){
+        result.value = 'broken'
+    }
 }
 </script>
